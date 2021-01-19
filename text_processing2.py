d984cde8eb180d2129c5af16de4c583f9346e188
SJY <ebbunnim@gmail.com>
2021-01-19 15:44:48 +0900

    Update my code

--- text_processing2.py
+++ text_processing2.py
@@ -2,6 +2,7 @@
 # Test Processing II  #
 #######################
 import re
+
 def digits_to_words(input_string):
     num_dict={
         '0':'zero',
@@ -18,19 +19,26 @@ def digits_to_words(input_string):
     digit_string=''
     for s in input_string:
         if s.isdigit():
-            print(s)
             digit_string+=num_dict[s]
             digit_string+=' '
     return digit_string
 
+
+def is_already_camel(x):
+    return x[0].islower()
+
+
 def to_camel_case(underscore_str):
+    print(underscore_str)
     res=re.split('[_?]',underscore_str)
-    res=[ele.lower() for ele in res if ele!='']
-    print(res)
+    res=[ele for ele in res if ele!='']
     camelcase_str=''
     if res:
-        camelcase_str+=res[0]
+        if is_already_camel(res[0]):
+            camelcase_str+=res[0]
+        else:
+            camelcase_str+=res[0].lower()
     for i in range(1,len(res)):
-        res[i]=res[i].capitalize()
+        res[i]=res[i].lower().capitalize()
         camelcase_str+=res[i]
     return camelcase_str
